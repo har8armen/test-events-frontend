d853013739b278a945f1e5728014eb5a56d55ed4
hovhannisyangagik <hovhannisyangagik64@gmail.com>
2023-12-12 18:10:38 +0400

    first commit

---
+++ src/components/Event/index.jsx
@@ -0,0 +1,26 @@
+import "./style.css";
+
+const Index = () => {
+    return (
+        <div id="events">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Date</th>
+                        <th>Location</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Index;
